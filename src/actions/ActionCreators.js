8036bda040b820c545799e01dc122006d1927332
Scott Young <young.scottw@gmail.com>
2017-04-19 13:58:02 -0500

    Updated the playersReducer and associated action creators

--- src/actions/ActionCreators.js
+++ src/actions/ActionCreators.js
@@ -1,8 +1,29 @@
-import { UPDATE_CITY } from '../actions/Actions'
+import { UPDATE_CITY, CREATE_CITY, UPDATE_PLAYER, CREATE_PLAYER } from '../actions/Actions'
 
 export default function updateCity(city){
     return {
         type: UPDATE_CITY,
         city
+    }
+}
+
+export default function createCity(city){
+    return {
+        type: CREATE_CITY,
+        city
+    }
+}
+
+export default function updatePlayer(player){
+    return {
+        type: UPDATE_PLAYER,
+        player
+    }
+}
+
+export default function createPlayer(player){
+    return {
+        type: CREATE_PLAYER,
+        player
     }
 }
--- src/reducers/PlayersReducer.js
+++ src/reducers/PlayersReducer.js
@@ -2,9 +2,9 @@ import { UPDATE_USER, CREATE_USER } from '../actions/Actions'
 
 export default function playersReducer(state=[], action){
     switch(action.type){
-        case CREATE_USER:
+        case CREATE_PLAYER:
             return [...state, action.state];
-        case UPDATE_USER:
+        case UPDATE_PLAYER:
             return state.forEach( player => {
                 if(player === action.player){
                     return Object.assign({}, state, player)
