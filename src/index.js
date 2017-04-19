68db54d6e9c3a3e47862a3b97039b6c0e0d9c11c
Scott Young <young.scottw@gmail.com>
2017-04-19 13:51:47 -0500

    Added store to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { createStore } from 'redux'
+import rootReducer from './reducers/RootReducer'
+
+let store = createStore(rootReducer)
 
 ReactDOM.render(
   <App />,
