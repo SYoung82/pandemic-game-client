d694e1f71b2db21ebffbd82d582e257a5ea4d3a4
Scott Young <young.scottw@gmail.com>
2017-04-20 11:28:15 -0500

    Added CurrentPlayerContainer to view and simple styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Board from './containers/Board'
+import CurrentPlayerContainer from './containers/CurrentPlayerContainer'
 
 class App extends Component {
   render() {
@@ -11,7 +12,12 @@ class App extends Component {
         <div id='board'>
           <Board />
         </div>
-        <div id='footer' />
+        <div id='footer'>
+          <CurrentPlayerContainer players={[{role: 'Medic',
+                                             currentPlayer: true,
+                                             currentCity: 'Atlanta'
+                                             }]}/>
+        </div>
       </div>
     );
   }
