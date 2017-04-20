443136bcafc87e43c1db63a6f3c5d3ea1fff78b6
Scott Young <young.scottw@gmail.com>
2017-04-20 08:22:31 -0500

    Fixed citiesReducer to update new state

--- src/reducers/CitiesReducer.js
+++ src/reducers/CitiesReducer.js
@@ -1,19 +1,18 @@
-import { UPDATE_CITY, CREATE_CITY } from '../actions/Actions'
+import { UPDATE_CITY } from '../actions/Actions'
 import { cities } from '../constants/Cities'
 
 export function citiesReducer( state = cities, action){
+
     switch(action.type){
-        case CREATE_CITY:
-            return [...state, action.state]
         case UPDATE_CITY:
             state.forEach( (city, index) => {
-                if(city.name === action.city.name) {
-                    let newState = state;
+                if(city.name === action.city.name) {;
+                    var newState = state;
                     newState[index] = action.city;
                     return newState;
+                    debugger;
                 }
             })
-            break;
         default:
             return state
     }
