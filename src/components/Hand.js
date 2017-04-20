3d4745beea1ee5e9bcc638b2cd23b5b3a9d314e4
Scott Young <young.scottw@gmail.com>
2017-04-20 15:36:21 -0500

    CSS style improvements

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -11,18 +11,22 @@ export default class Hand extends Component{
     }
     render() {
         var ulStyle = {
+            listStyleType: 'none',
             padding: 0,
             display: "inline-block", /*needed so that bottom border clears floats*/
             width: "100%"
         }
         var liStyle = {
-            width: '25%',
+            border: 'solid',
+            width: '13%',
             float: 'left', 
         }
         var currentHand = this.state.hand
+
         return(
             <ul style={ulStyle} className={'card'}>
                 {currentHand.map( (card, index) => {
+                    liStyle.backgroundColor = card.color
                     return <li style={liStyle} key={index}><Card card={card} /></li> 
                 })}
             </ul>
--- src/components/Player.js
+++ src/components/Player.js
@@ -4,16 +4,16 @@ import Hand from '../components/Hand';
 export default class Player extends Component{
     render() {
         return(
-            <div style={{display: 'flex'}}>
-                <div style={{flex: 1}}>
+            <div style={{display: 'inline-flex'}}>
+                <div style={{width: '10%'}}>
                     <h3>Current Player: {this.props.player.role}</h3>
                 </div>
-                <div className={'hand'} style={{flex: 1}}>
+                <div className={'hand'} style={{width: '90%'}}>
                     <Hand hand={this.props.player.hand}/>
                 </div>
-                <div style={{flex: 1}}>
+                {/*<div style={{flex: 1}}>
                     <p>Current City: {this.props.player.currentCity}</p>
-                </div>
+                </div>*/}
             </div>
         )
     }
