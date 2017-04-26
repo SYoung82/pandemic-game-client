8de2c8e5ecfca7af5ce90f64a12c9faf7676faa4
Scott Young <young.scottw@gmail.com>
2017-04-26 18:19:35 -0500

    Added end turn button to Player Component

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import Hand from '../components/Hand';
+import { connect } from 'react-redux';
 
-export default class Player extends Component{
+class Player extends Component{
+    onEndTurnClick(e) {
+        console.log('End Turn Clicked');
+        
+        this.props.dispatch({
+            type: 'NEXT_PLAYER'
+        })
+    }
     render() {
         return(
             <div style={{display: 'inline-flex'}}>
                 <div style={{width: '25%', lineHeight: '15px'}}>
                     <h3>Current Player: {this.props.player.role}</h3>
                     <p>{this.props.player.currentCity}</p>
+                    <button onClick={this.onEndTurnClick.bind(this)}>End Turn</button>
                 </div>
                 <div className={'hand'} style={{width: '100%'}}>
                     <Hand hand={this.props.player.hand}/>
@@ -16,3 +25,5 @@ export default class Player extends Component{
         )
     }
 }
+
+export default connect()(Player)
