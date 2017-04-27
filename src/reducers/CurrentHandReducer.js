172bc791f7a23fc6e21592282142a88d904b015c
Scott Young <young.scottw@gmail.com>
2017-04-27 16:02:08 -0500

    Added NEXT_PLAYER action handlers to currentHand and currentPlayer reducers

--- src/reducers/CurrentHandReducer.js
+++ src/reducers/CurrentHandReducer.js
@@ -2,7 +2,9 @@ import { players } from '../constants/Players';
 
 export function currentHandReducer(state=players.find(p => p.currentPlayer === true).hand, action) {
     
-    switch(action.type) {        
+    switch(action.type) {
+        case 'NEXT_PLAYER':
+            return players.find(p => p.currentPlayer === true).hand        
         default:
             return state;
     }
