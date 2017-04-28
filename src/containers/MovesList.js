a5107c81d1b9c5ef0d8de8d4d54506cfc88de35d
Scott Young <young.scottw@gmail.com>
2017-04-28 11:59:51 -0500

    Added fields to dispatch to store

--- src/containers/MovesList.js
+++ src/containers/MovesList.js
@@ -6,23 +6,32 @@ class MovesList extends Component {
         super(props);
 
         this.state = {
-            adjacentCities: this.props.adjacentCities
+            adjacentCities: this.props.adjacentCities,
+            currentPlayer: this.props.currentPlayer
         }
     }
 
     handleAdjacentCityClick(e) {
         e.preventDefault();
         console.log('City ' + e.target.innerText + ' clicked in AdjacentCities list' );
+
+        this.props.dispatch({
+            type: 'MOVE_PLAYER',
+            city: e.target.innerText,
+            currentPlayer: this.props.currentPlayer,
+            currentCity: this.props.currentPlayer.currentCity
+        })
     }
 
     render() {
         const listItems = this.props.adjacentCities.map( city => 
-            <li key={city}><a key={city} style={{color: 'white'}} href='#' onClick={this.handleAdjacentCityClick.bind(this)}>Move to {city}</a></li>
+            <li key={city}><a key={city} style={{color: 'white'}} href='#' onClick={this.handleAdjacentCityClick.bind(this)}>{city}</a></li>
         )
 
         return (
             <div>
                 <h3>Adjacent Cities:</h3>
+                <h5>Move to: </h5>
                 <ul>
                     {listItems}
                 </ul>
@@ -33,7 +42,8 @@ class MovesList extends Component {
 
 function mapStateToProps(state) {
     return {
-        adjacentCities: state.adjacentCitiesReducer
+        adjacentCities: state.adjacentCitiesReducer,
+        currentPlayer: state.currentPlayerReducer
     }
 }
 
