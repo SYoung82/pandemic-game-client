39915b0f8eecd4e8c7baad571b1917c261aaebc2
Scott Young <young.scottw@gmail.com>
2017-04-28 10:12:56 -0500

    Render adjacent cities in MovesList Component using adjacentCitiesReducer

--- src/containers/MovesList.js
+++ src/containers/MovesList.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class MovesList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            adjacentCities: this.props.adjacentCities
+        }
+    }
+
     render() {
-        return <div>Moves List:</div>
+        const listItems = this.props.adjacentCities.map( city => <li key={city}>{city}</li>)
+        return (
+            <div>
+                <h3>Adjacent Cities:</h3>
+                <ul>
+                    {listItems}
+                </ul>
+            </div>
+        )
     }
 }
 
-export default MovesList
+function mapStateToProps(state) {
+    return {
+        adjacentCities: state.adjacentCitiesReducer
+    }
+}
+
+export default connect(mapStateToProps)(MovesList)
--- src/reducers/AdjacentCitiesReducer.js
+++ src/reducers/AdjacentCitiesReducer.js
@@ -4,7 +4,6 @@ import { players } from '../constants/Players'
 const currentPlayer = players.find(p => p.currentPlayer === true)
 
 export function adjacentCitiesReducer(state = cities.find(c => c.name === currentPlayer.currentCity).adjacentCities, action) {
-    debugger
     switch(action.type) {   
         default:
             return state;
