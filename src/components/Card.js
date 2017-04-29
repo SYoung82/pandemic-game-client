a5352aa3d7cfe9246fce483bc88228de5b3d8fc8
Scott Young <young.scottw@gmail.com>
2017-04-29 09:37:07 -0500

    Pushed Card state up

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,30 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Card extends Component{
-    constructor(props){
-        super(props);
-
-        this.state = {
-            name: this.props.card.name,
-            color: this.props.card.color,
-            type: this.props.card.type,
-            population: this.props.card.population,
-            description: this.props.card.description
-        }
-    }
-
-
     handleClick(e){
-        console.log(`Clicked ${this.state.name} card. Color is ${this.state.color}`)
+        console.log(`Clicked ${this.props.card.name} card. Color is ${this.props.card.color}`)
     }
 
     handleDescriptionClick(e){
-        alert(`Description: ${this.state.description}`);
+        alert(`Description: ${this.props.card.description}`);
     }
 
     render() {
         const style = {
-            backgroundColor: this.state.color,
+            backgroundColor: this.props.card.color,
             height: '154px',
             fontSize: 'small',
             lineHeight: '15px',
@@ -36,26 +23,27 @@ class Card extends Component{
         if(style.backgroundColor === 'yellow'){
             style.backgroundColor = '#c6b900'
         }
-        if(!this.state.color){
+        if(!this.props.card.color){
             style.backgroundColor = 'green'
         }
 
         var pop = undefined;
-        if(this.state.population){
-            pop = this.state.population.toLocaleString();
+        if(this.props.card.population){
+            pop = this.props.card.population.toLocaleString();
         }
             
         return(
             
             <div>
                 <ul style={style} onClick={this.handleClick.bind(this)}>
-                    <li><h3>{this.state.name}</h3></li>
+                    <li><h3>{this.props.card.name}</h3></li>
                     {pop && <li>Population: {pop}</li>}
-                    {this.state.description && <li><a style={{color: 'white'}} href='#' onClick={this.handleDescriptionClick.bind(this)}>Description</a></li>}
+                    {this.props.card.description && <li><a style={{color: 'white'}} href='#' onClick={this.handleDescriptionClick.bind(this)}>Description</a></li>}
                 </ul>
             </div>
         )
     }
 }
+
 
 export default connect()(Card)
