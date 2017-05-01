3aafc4fd00a7838930726b6ee59f5ec82d71e311
Scott Young <young.scottw@gmail.com>
2017-05-01 11:21:17 -0500

    Handle NEXT_PLAYER in adjacentCitiesReducer

--- src/reducers/AdjacentCitiesReducer.js
+++ src/reducers/AdjacentCitiesReducer.js
@@ -6,6 +6,10 @@ const adjCitiesList = adjacentCities
 
 export function adjacentCitiesReducer(state = adjacentCities.find(c => c.name === currentPlayer.currentCity).adjacentCities, action) {
     switch(action.type) {
+        case 'NEXT_PLAYER':
+            currentPlayer = players.find(p => p.currentPlayer === true)
+            return adjacentCities.find( c => c.name === currentPlayer.currentCity).adjacentCities
+
         case 'MOVE_PLAYER':
             return adjCitiesList.find( city => city.name === currentPlayer.currentCity ).adjacentCities
 
