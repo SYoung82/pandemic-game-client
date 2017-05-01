a719f07b17909f17d4938d05f77a5e9379c440d5
Scott Young <young.scottw@gmail.com>
2017-05-01 15:36:42 -0500

    Added Fly to cities to MovesList and attached listener

--- src/containers/MovesList.js
+++ src/containers/MovesList.js
@@ -14,19 +14,41 @@ class MovesList extends Component {
         })
     }
 
+    handleFlyToCityClick(e) {
+        e.preventDefault();
+        console.log('City ' + e.target.innderText + ' clicked in FlyTo list' )
+
+        this.props.dispatch({
+            type: 'MOVE_PLAYER',
+            city: e.target.innerText,
+            currentPlayer: this.props.currentPlayer,
+            currentCity: this.props.currentPlayer.currentCity
+        })
+
+        this.props.dispatch({
+            type: 'DISCARD',
+            currentPlayer: this.props.currentPlayer,
+            card: e.target.innerText
+        })
+    }
+
     render() {
-        const listItems = this.props.adjacentCities.map( city => 
+        const moveToCities = this.props.adjacentCities.map( city => 
             <li key={city}><a key={city} style={{color: 'white'}} href='#' onClick={this.handleAdjacentCityClick.bind(this)}>{city}</a></li>
         )
 
+        const flyToCities = this.props.hand.map( card => 
+            <li key={card.name}><a key={card.name} style={{color: 'white'}} href='#' onClick={this.handleFlyToCityClick.bind(this)}>{card.name}</a></li>  
+        )
+        
         return (
             <div>
                 <h2>Current City: {this.props.currentPlayer.currentCity}</h2>
                 <h3>Adjacent Cities:</h3>
                 <h5>Move to: </h5>
-                <ul>
-                    {listItems}
-                </ul>
+                    <ul>{moveToCities}</ul>
+                <h5>Fly to: </h5>
+                    <ul>{flyToCities}</ul>
             </div>
         )
     }
@@ -35,7 +57,8 @@ class MovesList extends Component {
 function mapStateToProps(state) {
     return {
         adjacentCities: state.adjacentCitiesReducer,
-        currentPlayer: state.currentPlayerReducer
+        currentPlayer: state.currentPlayerReducer,
+        hand: state.currentHandReducer.filter( card => card.type === 'city')
     }
 }
 
