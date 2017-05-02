93cfbf93dba15d714798c30e7dfcf886c4298949
Scott Young <young.scottw@gmail.com>
2017-05-02 10:06:35 -0500

    Built prepInfectionDeck function

--- src/Game/Logic.js
+++ src/Game/Logic.js
@@ -1,3 +1,19 @@
+import { cityCards } from '../constants/Cards'
+
+export function prepInfectionDeck() {
+    let deck = cityCards
+    var shuffledDeck = []
+    for(let i=0; k<deck.length - 1, i++) {
+        shuffledDeck.push(arr.splice(Math.floor(Math.random()*deck.length),1)[0])
+    }
+    shuffledDeck.push(deck[0])
+    return shuffledDeck
+}
+
+export function prepPlayerDeck() {
+    
+}
+
 export function checkTurnOver(state) {
     if(state.movesLeft <= 1) {
         return true
