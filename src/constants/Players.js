48e0c012d832798ccb65f7440fa7879961bca496
Scott Young <young.scottw@gmail.com>
2017-05-02 12:33:34 -0500

    Updated players const to use empty hands initially

--- src/constants/Players.js
+++ src/constants/Players.js
@@ -1,16 +1,4 @@
 export const players = [
-    {id: 1, name: 'Player 1', role: 'Medic', color: 'orange', currentPlayer: true, movesLeft: 4, currentCity: 'Atlanta', hand: [{name: 'Lagos', color: 'yellow', population: 11547000, type: 'city'},
-                                                                                                                  {name: 'Washington', color: 'blue', population: 4679000, type: 'city'},
-                                                                                                                  {name: 'Santiago', color: 'yellow', population: 6015000, type: 'city'},
-                                                                                                                  {name: 'Milan', color: 'blue', population: 5232000, type: 'city'},
-                                                                                                                  {name: 'Ho Chi Minh City', color: 'red', population: 8314000, type: 'city'},
-                                                                                                                  {name: 'Riyadh', color: 'black', population: 5037000, type: 'city'},
-                                                                                                                  {name: 'Chicago', color: 'blue', population: 9121000, type: 'city'},
-                                                                                                                  {name: 'Chennai', color: 'black', population: 8865000, type: 'city'}]},
-    {id: 2, name: 'Player 2', role: 'Contingency Planner', color: 'dark green', currentPlayer: false, movesLeft: 4, currentCity: 'Atlanta', hand: [{name: 'Sao Paulo', color: 'yellow', population: 20186000, type: 'city'},
-                                                                                                                                     {name: 'Manila', color: 'red', population: 20767000, type: 'city'},
-                                                                                                                                     {name: 'Moscow', color: 'black', population: 15512000, type: 'city'},
-                                                                                                                                     {name: 'Osaka', color: 'red', population: 2871000, type: 'city'},
-                                                                                                                                     {name: 'St. Petersburg', color: 'blue', population: 4879000, type: 'city'},
-                                                                                                                                     {name: 'Cairo', color: 'black', population: 14718000, type: 'city'}]}
+    {id: 1, name: 'Player 1', role: 'Medic', color: 'orange', currentPlayer: true, movesLeft: 4, currentCity: 'Atlanta', hand: []},
+    {id: 2, name: 'Player 2', role: 'Contingency Planner', color: 'dark green', currentPlayer: false, movesLeft: 4, currentCity: 'Atlanta', hand: []}
 ]
