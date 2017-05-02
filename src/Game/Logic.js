7a5bb78ddfcada99fae128c4dab1105a9369cc8b
Scott Young <young.scottw@gmail.com>
2017-05-02 11:33:56 -0500

    Built prepPlayerDeck helper function to build and shuffle deck

--- src/Game/Logic.js
+++ src/Game/Logic.js
@@ -1,18 +1,38 @@
 export function prepInfectionDeck(cards) {
     var currentIndex = cards.length, temporaryValue, randomIndex
-
-    while( 0 !== currentIndex) {
-        randomIndex = Math.floor(Math.random() * currentIndex)
-        currentIndex -= 1
-        temporaryValue = cards[currentIndex]
-        cards[currentIndex] = cards[randomIndex]
-        cards[randomIndex] = temporaryValue
+    if(!cards.prepped) {
+        while( 0 !== currentIndex ) {
+            randomIndex = Math.floor(Math.random() * currentIndex)
+            currentIndex -= 1
+            temporaryValue = cards[currentIndex]
+            cards[currentIndex] = cards[randomIndex]
+            cards[randomIndex] = temporaryValue
+        }
+        cards.prepped = true
+        return cards
     }
     return cards
 }
 
-export function prepPlayerDeck() {
-    
+export function prepPlayerDeck(cards, difficulty=4) {
+    if(!cards.prepped) {
+        var completedDeck = cards
+        var currentIndex = cards.length, temporaryValue, randomIndex
+
+        while( 0 !== currentIndex ) {
+            randomIndex = Math.floor(Math.random() * currentIndex)
+            currentIndex -= 1
+            temporaryValue = completedDeck[currentIndex]
+            completedDeck[currentIndex] = completedDeck[randomIndex]
+            completedDeck[randomIndex] = temporaryValue
+        }
+        
+        insertEpidemicCards(completedDeck, difficulty)
+        completedDeck.prepped = true
+
+        return completedDeck
+    }
+    return cards
 }
 
 export function checkTurnOver(state) {
@@ -24,4 +44,18 @@ export function checkTurnOver(state) {
 
 export function checkGameOver(state) {
     return false
+}
+
+function randomIntFromInterval(min,max) {
+    return Math.floor(Math.random()*(max-min+1)+min)
+}
+
+function insertEpidemicCards(deck, difficulty = 4) {
+    var randomIndex
+    for(let j=0; j<difficulty; j++) {
+        randomIndex = randomIntFromInterval(j*12, (j*12)+12)
+        console.log(randomIndex)
+        console.log('j: ', j)
+        deck.splice(randomIndex, 0, {name: 'Epidemic', color: 'green', type: 'epidemic'})
+    }
 }
