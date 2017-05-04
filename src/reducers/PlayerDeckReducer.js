24e36ffe17bb82a67f29c0296e82ac22ed0f959b
Scott Young <young.scottw@gmail.com>
2017-05-04 16:51:57 -0500

    Built DRAW_PLAYER_CARDS case in playerDeckReducer

--- src/reducers/PlayerDeckReducer.js
+++ src/reducers/PlayerDeckReducer.js
@@ -3,6 +3,9 @@ import { prepPlayerDeck } from '../Game/Logic'
 
 export function playerDeckReducer(state = prepPlayerDeck(playerDeck, 4), action) {
     switch(action.type) {
+        case 'DRAW_PLAYER_CARDS':
+            return state.slice(2)
+
         default:
             return state
     }
