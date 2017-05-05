239b3a4c635f5def04dfba324206ca71f5cd7f4a
Scott Young <young.scottw@gmail.com>
2017-05-05 15:45:28 -0500

    Removed unnecessary props being passed to children

--- src/containers/MovesList.js
+++ src/containers/MovesList.js
@@ -14,8 +14,8 @@ class MovesList extends Component {
                 {this.props.currentCity.researchStation ? <h4>{"Research Station: ✓"}</h4> : null}
                 <h3>Moves Left: {this.props.currentPlayer.movesLeft}</h3>
                 <CureCubeLinks cubes={this.props.currentCity.cubes} currentCity={this.props.currentCity} currentPlayer={this.props.currentPlayer} playerDeck={this.props.playerDeck} />
-                <AdjacentCityLinks adjacentCities={this.props.currentCity.adjacentCities} currentCity={this.props.currentCity} currentPlayer={this.props.currentPlayer} playerDeck={this.props.playerDeck} />
-                <FlyToCityLinks currentCity={this.props.currentCity} currentPlayer={this.props.currentPlayer} playerDeck={this.props.playerDeck} />
+                <AdjacentCityLinks adjacentCities={this.props.currentCity.adjacentCities} currentPlayer={this.props.currentPlayer} playerDeck={this.props.playerDeck} />
+                <FlyToCityLinks currentPlayer={this.props.currentPlayer} playerDeck={this.props.playerDeck} />
                 <ResearchStationLinks researchStationCities={this.props.researchStationCities} currentPlayer={this.props.currentPlayer} currentCity={this.props.currentCity} playerDeck={this.props.playerDeck} />
                 <BuildResearchStationLink currentPlayer={this.props.currentPlayer} currentCity={this.props.currentCity} playerDeck={this.props.playerDeck} />
             </div>
@@ -25,7 +25,6 @@ class MovesList extends Component {
 
 function mapStateToProps(state) {
     return {
-        //adjacentCites: state.citiesReducer.find( city => city.name === state.playersReducer.find( player => player.currentPlayer === true).currentCity).adjacentCities,
         adjacentCities: state.adjacentCitiesReducer,
         currentPlayer: state.playersReducer.find( player => player.currentPlayer === true),
         researchStationCities: state.researchStationCitiesReducer,
