e7389d61d36659140266102fc633e6ea399a15f3
Scott Young <young.scottw@gmail.com>
2017-05-04 19:12:27 -0500

    Overhaul adjacentCitiesReducer

--- src/reducers/AdjacentCitiesReducer.js
+++ src/reducers/AdjacentCitiesReducer.js
@@ -1,16 +1,18 @@
 import { players } from '../constants/Players'
 import { adjacentCities } from '../constants/AdjacentCities'
 
-var currentPlayer = players.find(p => p.currentPlayer === true)
+var initialPlayer = players.find(p => p.currentPlayer === true)
 const adjCitiesList = adjacentCities
 
-export function adjacentCitiesReducer(state = adjacentCities.find(c => c.name === currentPlayer.currentCity).adjacentCities, action) {
+export function adjacentCitiesReducer(state = adjCitiesList.find( city => city.name === initialPlayer.currentCity).adjacentCities, action) {
     switch(action.type) {
         case 'NEXT_PLAYER':
-            currentPlayer = players.find(p => p.currentPlayer === true)
-            return adjacentCities.find( c => c.name === currentPlayer.currentCity).adjacentCities
+            var currentPlayer = action.currentPlayer
+            debugger
+            return adjCitiesList.find( c => c.name === currentPlayer.currentCity).adjacentCities
 
         case 'MOVE_PLAYER':
+            currentPlayer = action.currentPlayer
             return adjCitiesList.find( city => city.name === currentPlayer.currentCity ).adjacentCities
 
         default:
