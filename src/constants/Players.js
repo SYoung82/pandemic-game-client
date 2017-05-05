e6d16623b249ccc0d87e662934fcd1b9f1ad852b
Scott Young <young.scottw@gmail.com>
2017-05-05 12:26:54 -0500

    Amended player 2 color

--- src/constants/Players.js
+++ src/constants/Players.js
@@ -1,4 +1,4 @@
 export const players = [
     {id: 1, name: 'Player 1', role: 'Medic', color: 'orange', currentPlayer: true, movesLeft: 4, currentCity: 'Atlanta', hand: []},
-    {id: 2, name: 'Player 2', role: 'Contingency Planner', color: 'dark green', currentPlayer: false, movesLeft: 4, currentCity: 'Atlanta', hand: []}
+    {id: 2, name: 'Player 2', role: 'Contingency Planner', color: 'dark-green', currentPlayer: false, movesLeft: 4, currentCity: 'Atlanta', hand: []}
 ]
