e9a5a9ed83d55726bbee4e308a59f5e17a66b835
Scott Young <young.scottw@gmail.com>
2017-05-05 15:39:43 -0500

    Updated AdjCityLinks component to be give different props for adjacentCities

--- src/containers/MovesList.js
+++ src/containers/MovesList.js
@@ -7,14 +7,14 @@ import ResearchStationLinks from '../components/ResearchStationLinks'
 import BuildResearchStationLink from '../components/BuildResearchStationLink'
 
 class MovesList extends Component {
-    render() {
+    render() { 
         return (
             <div>
                 <h2>Current City: {this.props.currentPlayer.currentCity}</h2>
                 {this.props.currentCity.researchStation ? <h4>{"Research Station: ✓"}</h4> : null}
                 <h3>Moves Left: {this.props.currentPlayer.movesLeft}</h3>
                 <CureCubeLinks cubes={this.props.currentCity.cubes} currentCity={this.props.currentCity} currentPlayer={this.props.currentPlayer} playerDeck={this.props.playerDeck} />
-                <AdjacentCityLinks adjacentCities={this.props.adjacentCities} currentCity={this.props.currentCity} currentPlayer={this.props.currentPlayer} playerDeck={this.props.playerDeck} />
+                <AdjacentCityLinks adjacentCities={this.props.currentCity.adjacentCities} currentCity={this.props.currentCity} currentPlayer={this.props.currentPlayer} playerDeck={this.props.playerDeck} />
                 <FlyToCityLinks currentCity={this.props.currentCity} currentPlayer={this.props.currentPlayer} playerDeck={this.props.playerDeck} />
                 <ResearchStationLinks researchStationCities={this.props.researchStationCities} currentPlayer={this.props.currentPlayer} currentCity={this.props.currentCity} playerDeck={this.props.playerDeck} />
                 <BuildResearchStationLink currentPlayer={this.props.currentPlayer} currentCity={this.props.currentCity} playerDeck={this.props.playerDeck} />
