19d166ab1e3283abb61bc5e635f36f6987a5d80a
Scott Young <young.scottw@gmail.com>
2017-05-05 15:28:30 -0500

    Fixed adjacentCitiesReducer from returning wrong list of cities

--- src/reducers/AdjacentCitiesReducer.js
+++ src/reducers/AdjacentCitiesReducer.js
@@ -1,5 +1,6 @@
 import { players } from '../constants/Players'
 import { adjacentCities } from '../constants/AdjacentCities'
+import update from 'react-addons-update';
 
 var initialPlayer = players.find(p => p.currentPlayer === true)
 const adjCitiesList = adjacentCities
@@ -7,8 +8,9 @@ const adjCitiesList = adjacentCities
 export function adjacentCitiesReducer(state = adjCitiesList.find( city => city.name === initialPlayer.currentCity).adjacentCities, action) {
     switch(action.type) {
         case 'NEXT_PLAYER':
-            var currentPlayer = action.currentPlayer
-            return adjCitiesList.find( c => c.name === currentPlayer.currentCity).adjacentCities
+            var currentPlayer = players.find(player => player.name !== action.currentPlayer.name)
+            debugger
+            return update(state, {$set: adjCitiesList.find( city => city.name === currentPlayer.currentCity).adjacentCities})
 
         case 'MOVE_PLAYER':
             currentPlayer = action.currentPlayer
