c3f6706a70acacd3a0d53c5ebb25811abb3e85e1
Scott Young <young.scottw@gmail.com>
2017-05-05 12:27:15 -0500

    Add player pawns to map

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -1,9 +1,12 @@
 
 import React, { Component } from 'react';
-import { Map, TileLayer, Polyline } from 'react-leaflet';
+import { Map, TileLayer, Polyline, Marker } from 'react-leaflet';
+import { Icon } from 'leaflet'
 import { paths } from '../constants/Paths'
 import CityMarker from '../components/CityMarker'
 import { connect } from 'react-redux'
+import darkGreenPawn from '../images/dark-green-pawn.png'
+import orangePawn from '../images/orange-pawn.png'
 
 class Board extends Component{
     state = {
@@ -14,6 +17,14 @@ class Board extends Component{
 
     render(){
         const position = [this.state.lat, this.state.lng];
+        var playerCities = []
+        this.props.players.forEach( player => {
+            if( player.color === 'orange' ) {
+                playerCities.push([this.props.cities.find( city => city.name === player.currentCity), orangePawn])
+            } else if ( player.color === 'dark-green') {
+                playerCities.push([this.props.cities.find( city => city.name === player.currentCity), darkGreenPawn])
+            }
+        })
 
         return(
             <Map center={position} zoom={this.state.zoom} zoomControl={false} dragging={false}>
@@ -25,11 +36,29 @@ class Board extends Component{
                     minZoom={this.state.zoom}
                     maxZoom={this.state.zoom}
                 />
+
                 {this.props.cities.map( (city) => {
                     return (
                         <CityMarker city={city} players={this.props.players} key={city.name} />
                     )
                 })}
+
+                {playerCities.map( (city, index) => {
+                    return(
+                        <Marker position={index % 2 === 0 ? [city[0].lat+(index+3), city[0].lng+(index+3)] : [city[0].lat+(index-3), city[0].lng+(index-3)]} 
+                                key={index} 
+                                icon={
+                                    new Icon({iconUrl: city[1],
+                                          iconSize: [25,25]   
+                                    })
+                                }
+                                dragging={false}
+                                zIndexOffset={1000} 
+                        />
+                    )
+                })
+                }
+
                 {paths.map( (path, index) => {
                     return (
                         <Polyline key={index} color='black' positions={path} />
