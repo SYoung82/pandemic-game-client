43ccf07bb0479055b06a0622bfec869343b165b5
Scott Young <young.scottw@gmail.com>
2017-05-07 11:35:14 -0500

    Created About component

---
+++ src/containers/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return(
+            <div className='about'>
+                <h1>About:</h1>
+                <ul>
+                    <li>v 0.1</li>
+                    <li>Created by Scott Young</li>
+                    <li><a href='https://www.github.com/SYoung82'>https://www.github.com/SYoung82</a></li>
+                </ul>
+            </div>)
+    }
+}
