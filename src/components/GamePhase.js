38ab1cda5cf802d966a5c62268dd97279f322c05
Scott Young <young.scottw@gmail.com>
2017-05-08 15:13:37 -0500

    Remove unnused variable

--- src/components/GamePhase.js
+++ src/components/GamePhase.js
@@ -3,11 +3,10 @@ import { connect } from 'react-redux'
 
 class GamePhase extends Component {
     render() {
-        const blackStatus = this.props.gameStatus.black.charAt(0).toUpperCase()
         return(
             <div>
                 Current Phase: {this.props.gameStatus.phase},
-                 Black: {this.props.gameStatus.black},
+                     Black: {this.props.gameStatus.black},
                   Red: {this.props.gameStatus.red},
                    Blue: {this.props.gameStatus.blue},
                     Yellow: {this.props.gameStatus.yellow}
