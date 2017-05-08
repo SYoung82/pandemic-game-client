5ec336a3e05e831d29ef713ef84285b017bd4eea
Scott Young <young.scottw@gmail.com>
2017-05-08 12:47:19 -0500

    Revert to priming App with dummy characters

--- src/App.js
+++ src/App.js
@@ -19,7 +19,31 @@ class App extends Component {
           </div>
         </div>
         <div id='footer'>
-          <CurrentPlayerContainer />
+          <CurrentPlayerContainer players={[{role: 'Medic',
+                                             currentPlayer: true,
+                                             currentCity: 'Atlanta',
+                                             hand: [{name: 'Lagos', color: 'yellow', population: 11547000, type: 'city'},
+                                                    {name: 'Washington', color: 'blue', population: 4679000, type: 'city'},
+                                                    {name: 'Santiago', color: 'yellow', population: 6015000, type: 'city'},
+                                                    {name: 'Milan', color: 'blue', population: 5232000, type: 'city'},
+                                                    {name: 'Ho Chi Minh City', color: 'red', population: 8314000, type: 'city'},
+                                                    {name: 'Riyadh', color: 'black', population: 5037000, type: 'city'},
+                                                    {name: 'Chicago', color: 'blue', population: 9121000, type: 'city'},
+                                                    {name: 'Chennai', color: 'black', population: 8865000, type: 'city'},
+                                                    {name: 'Airlift', description: "Move any 1 pawn to any city. Get permssion before moving another player's pawn.", type: 'event'}]
+                                             },
+                                             {role: 'Contingency Planner',
+                                             currentPlayer: false,
+                                             currentCity: 'Atlanta',
+                                             hand: [{name: 'Sao Paulo', color: 'yellow', population: 20186000, type: 'city'},
+                                                    {name: 'Manila', color: 'red', population: 20767000, type: 'city'},
+                                                    {name: 'Moscow', color: 'black', population: 15512000, type: 'city'},
+                                                    {name: 'Osaka', color: 'red', population: 2871000, type: 'city'},
+                                                    {name: 'St. Petersburg', color: 'blue', population: 4879000, type: 'city'},
+                                                    {name: 'Cairo', color: 'black', population: 14718000, type: 'city'},
+                                                    {name: 'Montreal', color: 'blue', population: 3429000, type: 'city'}]
+                                             }
+                                             ]}/>
         </div>
       </div>
     );
