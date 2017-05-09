4881d34a959dd340a25cfb52e076cd14603fccd6
Scott Young <young.scottw@gmail.com>
2017-05-09 12:40:38 -0500

    Removed debugger

--- src/reducers/InfectionDeckReducer.js
+++ src/reducers/InfectionDeckReducer.js
@@ -5,7 +5,6 @@ import { prepInfectionDeck } from '../Game/Logic'
 export function infectionDeckReducer(state = prepInfectionDeck(cityCards), action) {
     switch(action.type) {
         case 'DRAW_BOTTOM_INFECTION_CARD':
-        debugger
             return state.splice(0, state.length-1)
 
         default:
