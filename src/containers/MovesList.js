7662e032ac0297b8007c45fbe354fe955f902292
Scott Young <young.scottw@gmail.com>
2017-05-09 11:43:03 -0500

    Formatting for easier reading

--- src/containers/MovesList.js
+++ src/containers/MovesList.js
@@ -14,12 +14,35 @@ class MovesList extends Component {
                 <h2>Current City: {this.props.currentPlayer.currentCity}</h2>
                 {this.props.currentCity.researchStation ? <h4>{"Research Station: ✓"}</h4> : null}
                 <h3>Moves Left: {this.props.currentPlayer.movesLeft}</h3>
-                <CureDiseaseLinks currentPlayer={this.props.currentPlayer} currentCity={this.props.currentCity} hand={this.props.currentPlayer.hand} gameStatus={this.props.gameStatus} playerDeck={this.props.playerDeck} />
-                <CureCubeLinks cubes={this.props.currentCity.cubes} currentCity={this.props.currentCity} currentPlayer={this.props.currentPlayer} playerDeck={this.props.playerDeck} />
-                <AdjacentCityLinks adjacentCities={this.props.currentCity.adjacentCities} currentPlayer={this.props.currentPlayer} playerDeck={this.props.playerDeck} infectionDeck={this.props.infectionDeck} />
-                <FlyToCityLinks currentPlayer={this.props.currentPlayer} hand={this.props.currentPlayer.hand} playerDeck={this.props.playerDeck} />
-                <ResearchStationLinks researchStationCities={this.props.researchStationCities} currentPlayer={this.props.currentPlayer} currentCity={this.props.currentCity} playerDeck={this.props.playerDeck} />
-                <BuildResearchStationLink currentPlayer={this.props.currentPlayer} currentCity={this.props.currentCity} playerDeck={this.props.playerDeck} />
+                <CureDiseaseLinks 
+                    currentPlayer={this.props.currentPlayer} 
+                    currentCity={this.props.currentCity} 
+                    hand={this.props.currentPlayer.hand} 
+                    gameStatus={this.props.gameStatus} 
+                    playerDeck={this.props.playerDeck} />
+                <CureCubeLinks 
+                    cubes={this.props.currentCity.cubes} 
+                    currentCity={this.props.currentCity} 
+                    currentPlayer={this.props.currentPlayer} 
+                    playerDeck={this.props.playerDeck} />
+                <AdjacentCityLinks 
+                    adjacentCities={this.props.currentCity.adjacentCities} 
+                    currentPlayer={this.props.currentPlayer} 
+                    playerDeck={this.props.playerDeck} 
+                    infectionDeck={this.props.infectionDeck} />
+                <FlyToCityLinks 
+                    currentPlayer={this.props.currentPlayer} 
+                    hand={this.props.currentPlayer.hand} 
+                    playerDeck={this.props.playerDeck} />
+                <ResearchStationLinks 
+                    researchStationCities={this.props.researchStationCities} 
+                    currentPlayer={this.props.currentPlayer}
+                    currentCity={this.props.currentCity} 
+                    playerDeck={this.props.playerDeck} />
+                <BuildResearchStationLink 
+                    currentPlayer={this.props.currentPlayer} 
+                    currentCity={this.props.currentCity} 
+                    playerDeck={this.props.playerDeck} />
             </div>
         )
     }
