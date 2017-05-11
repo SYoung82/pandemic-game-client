76b337a87856fc80c852cdc6f7f04b929a7a2ec8
Scott Young <young.scottw@gmail.com>
2017-05-11 10:08:39 -0500

    Handle END_GAME action

--- src/reducers/GameStatusReducer.js
+++ src/reducers/GameStatusReducer.js
@@ -1,6 +1,6 @@
 import update from 'react-addons-update'
 
-export function gameStatusReducer(state={red: 'Uncured', black: 'Uncured', blue: 'Uncured', yellow: 'Uncured', phase: 'Move'}, action) {
+export function gameStatusReducer(state={red: 'Uncured', black: 'Uncured', blue: 'Uncured', yellow: 'Uncured', phase: 'Move', isGameOver: false}, action) {
 
     switch(action.type) {
         case 'CURE_DISEASE':
@@ -8,6 +8,9 @@ export function gameStatusReducer(state={red: 'Uncured', black: 'Uncured', blue:
 
         case 'SET_GAME_PHASE':
             return update(state, {phase: {$set: action.phase}})
+
+        case 'END_GAME':
+            return update(state, {isGameOver: {$set: true}})
         
         default:
             return state
