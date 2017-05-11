fd9ed99d2935b3a31476432c372eef8a245cd81f
Scott Young <young.scottw@gmail.com>
2017-05-11 11:09:24 -0500

    Handle DRAW_INFECTION_CARDS in infectionDeckReducer

--- src/reducers/InfectionDeckReducer.js
+++ src/reducers/InfectionDeckReducer.js
@@ -7,6 +7,9 @@ export function infectionDeckReducer(state = prepInfectionDeck(cityCards), actio
         case 'DRAW_BOTTOM_INFECTION_CARD':
             return state.splice(0, state.length-1)
 
+        case 'DRAW_INFECTION_CARDS':
+            return state.splice(action.number, state.length-1)
+            
         default:
             return state
     }
