46bb5c1aa349617dea5410fd8c978b2e735c746c
Scott Young <young.scottw@gmail.com>
2017-05-12 14:06:14 -0500

    Wrap LoginForm in component for connection to redux state

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,31 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { Form, Text } from 'react-form'
+import { login } from '../actions/ActionCreators'
 
-const LoginForm = (
-    <Form
-        onSubmit={ values => console.log('Success!', values)}
+class LoginFormComponent extends Component {
+    render() { 
+        const LoginForm = <Form
+            onSubmit={ values => {
+                const { email, password } = values
 
-        validate={ values => {
-            const { email, password } = values
-            
-            return {
-                email: !email ? 'An email is required' : undefined,
-                password: !password ? 'A password is required' : undefined
-            }
-        }}
-    >
+                console.log('Success!', values)
+                this.props.dispatch(login(email, password))
+            }}
 
-    {({submitForm}) => {
-      return (
-        <form id='login-form' onSubmit={submitForm} style={{marginLeft: '15%', float: 'left'}}>
-            <h1>Login</h1>
-            <Text field='email' type='text' placeholder='Email' /><br />
-            <Text field='password' type='password' placeholder='Password' /><br />
-            <button id='signup-button' type='submit'>Submit</button>
-        </form>
-      )
-    }}
-    </Form>
-)
+            validate={ values => {
+                const { email, password } = values
+                        
+                return {
+                    email: !email ? 'An email is required' : undefined,
+                     password: !password ? 'A password is required' : undefined
+                }
+            }}
+        >
 
-export default LoginForm
+            {({submitForm}) => {
+            return (
+                <form id='login-form' onSubmit={submitForm} style={{marginLeft: '15%', float: 'left'}}>
+                    <h1>Login</h1>
+                    <Text field='email' type='text' placeholder='Email' /><br />
+                    <Text field='password' type='password' placeholder='Password' /><br />
+                    <button id='signup-button' type='submit'>Submit</button>
+                </form>
+            )
+            }}
+        </Form>
+
+        return(LoginForm)
+    }
+}
+
+export default connect()(LoginFormComponent)
