a683bdecfb2dfbb2586876e79293265d7ff759b8
Scott Young <young.scottw@gmail.com>
2017-05-11 21:37:34 -0500

    Disable End Turn button if not applicable

--- src/components/Player.js
+++ src/components/Player.js
@@ -13,7 +13,7 @@ class Player extends Component{
             <div style={{display: 'inline-flex'}}>
                 <div style={{width: '25%', lineHeight: '15px'}}>
                     <h3>Current Player: {this.props.player.role}</h3>
-                    <button onClick={this.onEndTurnClick.bind(this)}>End Turn</button>
+                    <button onClick={this.onEndTurnClick.bind(this)} disabled={this.props.gameStatus.phase !== 'Move'}>End Turn</button>
                 </div>
                 <div className={'hand'} style={{width: '100%'}}>
                     <Hand hand={this.props.hand} player={this.props.player} playerDeck={this.props.playerDeck} infectionDeck={this.props.infectionDeck} gameStatus={this.props.gameStatus} />
