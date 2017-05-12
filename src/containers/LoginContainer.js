c234735d5450b2eafb9298a32ccc53ed7bf72401
Scott Young <young.scottw@gmail.com>
2017-05-12 14:07:19 -0500

    Modified container to render the new Form wrappers

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
-import LoginForm from '../components/Login'
-import SignupForm from '../components/Signup'
+import LoginFormContainer from '../components/Login'
+import SignupFormContainer from '../components/Signup'
+
 
 class LoginContainer extends Component {
     render() {
         return (
             <div id='login-container' style={{marginTop: '10%'}}>
                 <h1 style={{textAlign: 'center'}}>Fandemic</h1>
-                {LoginForm}
-                {SignupForm}
+                <LoginFormContainer />
+                <SignupFormContainer />
             </div>
         )
     }
