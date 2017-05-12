75c14b893a4a965abd9d414892c8a17e9c7418e1
Scott Young <young.scottw@gmail.com>
2017-05-12 11:35:10 -0500

    Use newly refactored forms in LoginContainer

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
-import Login from '../components/Login'
-import Signup from '../components/Signup'
+import LoginForm from '../components/Login'
+import SignupForm from '../components/Signup'
 
 class LoginContainer extends Component {
     render() {
         return (
-            <div id='login-container' style={{marginTop: '15%'}}>
+            <div id='login-container' style={{marginTop: '10%'}}>
                 <h1 style={{textAlign: 'center'}}>Fandemic</h1>
-                <Login style={{float: 'left'}}/>
-                <Signup style={{float: 'left'}}/>
+                {LoginForm}
+                {SignupForm}
             </div>
         )
     }
