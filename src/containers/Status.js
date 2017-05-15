1e71e6d010dfa70cb6dd85a4badf995b4d59b1ba
Scott Young <young.scottw@gmail.com>
2017-05-15 11:02:59 -0500

    extract functionality that was placed in NavHeader

--- src/containers/Status.js
+++ src/containers/Status.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import GamePhase from '../components/GamePhase'
-import { saveGame } from '../actions/ActionCreators'
+
 
 class Status extends Component {
-    onSaveClick(e) {
-        this.props.dispatch(saveGame(this.props.gameStatus.id, this.props.gameStatus.token))
-    }
-
     render() {
         return(
             <div>
-                <button onClick={this.onSaveClick.bind(this)} >Save Game</button><br />
                 <GamePhase gameStatus={this.props.gameStatus} />
             </div>
         )
