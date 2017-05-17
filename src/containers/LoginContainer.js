9587eb78aad84290084fc59d5d433335435ff683
Scott Young <young.scottw@gmail.com>
2017-05-17 12:24:06 -0500

    Change title

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -8,7 +8,7 @@ class LoginContainer extends Component {
     render() {
         return (
             <div id='login-container' style={{marginTop: '10%'}}>
-                <h1 style={{textAlign: 'center'}}>Fandemic</h1>
+                <h1 style={{textAlign: 'center'}}>Pandemic</h1>
                 <LoginFormComponent isLoggedIn={this.props.isLoggedIn} />
                 <SignupFormComponent isLoggedIn={this.props.isLoggedIn} />
             </div>
